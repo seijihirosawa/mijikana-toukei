95712fd6379110427b7a6ffc0734439b519117df
seijihirosawa <seiji.hirosawa.0226@gmail.com>
2022-03-16 02:48:15 +0900

    Add files via upload

---
+++ seikibunpu.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import numpy as np
+from scipy.stats import norm
+import matplotlib.pyplot as plt
+import japanize_matplotlib
+
+
+st.title('身近な統計 補助教材')
+st.header("正規分布")
+
+st.write("""
+### 平均μと標準偏差σ（>0）をスライドバーで入力してください。
+""")
+
+m = st.slider('平均μ：', -10, 10, 0)
+
+s = st.slider('標準偏差σ', 0, 10, 1)
+
+# グラフの描画
+x = np.arange(-4, 4, 0.1)
+y = norm.pdf(x, m, s)
+
+fig, ax = plt.subplots()
+ax.plot(x, y)
+# x 軸のラベルを設定する。
+ax.set_xlabel("確率変数")
+# y 軸のラベルを設定する。
+ax.set_ylabel("確率密度")
+st.pyplot(fig)
